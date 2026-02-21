a5d61c971af33470b816639a2785120c7f681edc
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-21 19:50:38 +0530

    fixed name

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from brain import Brain
 from rich import print
+from actions import Actions
 
 def main():
 
     jarvis = Brain()
+    actions = Actions()
 
-    print("[bold cyan]Jarvis initialized.[/bold cyan]")
+    print("[bold cyan]Cognix initialized.[/bold cyan]")
     print("[bold yellow]Type 'exit' to quit.[/bold yellow]\n")
 
     while True:
@@ -13,12 +15,19 @@ def main():
         user_input = input()
 
         if user_input.lower() == "exit":
-            print("[bold red]Jarvis shutting down.[/bold red]")
+            print("[bold red]Cognix shutting down.[/bold red]")
             break
 
-        print("[bold cyan]Jarvis:[/bold cyan] ", end="")
+        action_result = actions.execute(user_input)
 
+        if action_result:
+            print("[bold cyan]Cognix:[/bold cyan] ", end="")
+            print(action_result)
+            continue
+
+        print("[bold cyan]Cognix:[/bold cyan] ", end="")
         jarvis.think(user_input)
+
         print()
 
 
